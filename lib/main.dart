d9b931439d7de58ac772bb3715de72c88c6ee6a5
ntsts03 <ntsts03@gmail.com>
2022-11-14 14:57:19 +0900

    appbar作成

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: hotcookCalculator(),
+    );
   }
 }
 
@@ -22,6 +25,10 @@ class hotcookCalculator extends StatefulWidget {
 class _hotcookCalculatorState extends State<hotcookCalculator> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('煮物塩分濃度計'),
+      ),
+    );
   }
 }
